98b4883e7cfbd660d1d9e563d0c77ac9c31336b8
joozahh <joozah@mail.ru>
2024-02-08 02:30:16 +0300

    final commit with no any non-constructor dependecies

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
     private Integer id;
 
